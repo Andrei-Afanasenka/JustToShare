b911cb8b83de15205ef8c41500e590b19494bb44
Andrei-Afanasenka <tolerance1985@mail.ru>
2017-05-31 13:46:22 +0300

    Initial check-in.

---
+++ HomeController.cs
@@ -0,0 +1,376 @@
+﻿namespace Project.Controllers
+{
+	#region Usings
+
+	using System;
+	using System.Collections.Generic;
+	using System.Diagnostics.Contracts;
+	using System.Linq;
+	using System.Text;
+	using System.Web.Mvc;
+
+	using Project.BusinessComponents.Interfaces;
+	using Project.BusinessComponents.Interfaces.Services;
+
+	using Project.BusinessEntities.DataContracts;
+	using Project.BusinessEntities.DataContracts.Enumerations;
+	using Project.BusinessEntities.Requests;
+
+	using Project.ViewModels.Home;
+
+	#endregion
+
+	/// <summary>
+	/// Home controller.
+	/// </summary>
+	public class HomeController : Controller
+	{
+		#region Private Fields
+
+		/// <summary>
+		/// Reference to the <see cref="IHomeService" /> implementer.
+		/// </summary>
+		private readonly IHomeService homeService;
+
+		/// <summary>
+		/// Reference to the <see cref="ILogger" /> implementer.
+		/// </summary>
+		private readonly ILogger logger;
+
+		/// <summary>
+		/// Reference to the <see cref="ICommonService" /> implementer.
+		/// </summary>
+		private readonly ICommonService commonService;
+
+		#endregion
+
+		#region Constructors
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="HomeController" /> class.
+		/// </summary>
+		/// <param name="homeService">The home service.</param>
+		/// <param name="logger">The logger.</param>
+		/// <param name="commonService">The common service.</param>
+		public HomeController(
+			IHomeService homeService,
+			ILogger logger,
+			ICommonService commonService)
+		{
+			Contract.Requires(homeService != null, "homeService");
+			Contract.Requires(logger != null, "logger");
+			Contract.Requires(commonService != null, "commonService");
+
+			this.homeService = homeService;
+			this.logger = logger;
+			this.commonService = commonService;
+		}
+
+		#endregion
+
+		#region Action Methods
+
+		/// <summary>
+		/// Index action.
+		/// </summary>
+		/// <returns>
+		/// Action result.
+		/// </returns>
+		/// <remarks>
+		/// GET: /Home/
+		/// </remarks>
+		public ActionResult Index()
+		{
+			return this.View();
+		}
+
+		/// <summary>
+		/// About action.
+		/// </summary>
+		/// <returns>
+		/// Action result.
+		/// </returns>
+		/// <remarks>
+		/// GET: /Home/About/
+		/// </remarks>
+		public ActionResult About()
+		{
+			return this.View();
+		}
+
+		/// <summary>
+		/// Contacts action.
+		/// </summary>
+		/// <returns>
+		/// Action result.
+		/// </returns>
+		/// <remarks>
+		/// GET: /Home/Contact/
+		/// </remarks>
+		public ActionResult Contacts()
+		{
+			return this.View(this.homeService.GetContacts());
+		}
+
+		/// <summary>
+		/// GetPublishedMessages action.
+		/// </summary>
+		/// <returns>
+		/// Action result.
+		/// </returns>
+		/// <remarks>
+		/// GET: /Home/GetPublishedMessages
+		/// </remarks>
+		public JsonResult GetPublishedMessages()
+		{
+			return this.Json(this.homeService.GetPublishedMessages(), JsonRequestBehavior.AllowGet);
+		}
+
+		/// <summary>
+		/// GetNews action.
+		/// </summary>
+		/// <returns>
+		/// Action result.
+		/// </returns>
+		/// <remarks>
+		/// GET: /Home/GetNews
+		/// </remarks>
+		public JsonResult GetNews()
+		{
+			return this.Json(this.homeService.GetArticles(ArticleType.News), JsonRequestBehavior.AllowGet);
+		}
+
+		/// <summary>
+		/// GetFAQ action.
+		/// </summary>
+		/// <returns>
+		/// Action result.
+		/// </returns>
+		/// <remarks>
+		/// GET: /Home/GetFAQ
+		/// </remarks>
+		public JsonResult GetFAQ()
+		{
+			return this.Json(this.homeService.GetArticles(ArticleType.FAQ), JsonRequestBehavior.AllowGet);
+		}
+
+		/// <summary>
+		/// Get Header Articles action.
+		/// </summary>
+		/// <returns>
+		/// Action result.
+		/// </returns>
+		/// <remarks>
+		/// GET: /Home/GetHeaderArticles
+		/// </remarks>
+		public JsonResult GetHeaderArticles()
+		{
+			return this.Json(this.homeService.GetArticles(ArticleType.HeaderArticle), JsonRequestBehavior.AllowGet);
+		}
+
+		/// <summary>
+		/// GetRouteInfo action.
+		/// </summary>
+		/// <returns>
+		/// Action result.
+		/// </returns>
+		/// <remarks>
+		/// GET: /Home/GetRouteInfo
+		/// </remarks>
+		public JsonResult GetRouteInfo()
+		{
+			return this.Json(this.homeService.GetRouteInfo(), JsonRequestBehavior.AllowGet);
+		}
+
+		/// <summary>
+		/// News action.
+		/// </summary>
+		/// <returns>
+		/// Action result.
+		/// </returns>
+		/// <remarks>
+		/// GET: /Home/News/id
+		/// </remarks>
+		public ActionResult News(string id)
+		{
+			if (string.IsNullOrWhiteSpace(id))
+			{
+				return this.View("Index");
+			}
+
+			Article article = this.homeService.GetArticle(ArticleType.News, id);
+
+			return (article != null) ? this.View(article) : this.View("Index");
+		}
+
+		/// <summary>
+		/// FAQ action.
+		/// </summary>
+		/// <returns>
+		/// Action result.
+		/// </returns>
+		/// <remarks>
+		/// GET: /Home/FAQ/id
+		/// </remarks>
+		public ActionResult FAQ(string id)
+		{
+			if (string.IsNullOrWhiteSpace(id))
+			{
+				return this.View("Index");
+			}
+
+			Article article = this.homeService.GetArticle(ArticleType.FAQ, id);
+
+			return (article != null) ? this.View(article) : this.View("Index");
+		}
+
+		/// <summary>
+		/// Header Articles action.
+		/// </summary>
+		/// <returns>
+		/// Action result.
+		/// </returns>
+		/// <remarks>
+		/// GET: /Home/HeaderArticles/id
+		/// </remarks>
+		public ActionResult HeaderArticles(string id)
+		{
+			if (string.IsNullOrWhiteSpace(id))
+			{
+				return this.View("Index");
+			}
+
+			Article article = this.homeService.GetArticle(ArticleType.HeaderArticle, id);
+
+			return (article != null) ? this.View(article) : this.View("Index");
+		}
+
+		/// <summary>
+		/// Route action.
+		/// </summary>
+		/// <param name="id">The identifier.</param>
+		/// <param name="date">The date.</param>
+		/// <returns>
+		/// Action result.
+		/// </returns>
+		/// <remarks>
+		/// GET: /Home/Route/id/date
+		/// </remarks>
+		public ActionResult Route(string id, string date)
+		{
+			if (string.IsNullOrWhiteSpace(id))
+			{
+				return this.View("Index");
+			}
+
+			RouteInfo routeInfo = this.homeService.GetRouteInfo(id);
+			
+			if (routeInfo == null)
+			{
+				return this.View("Index");
+			}
+			
+			return this.View("Search", new SearchViewModel
+			{
+				From = routeInfo.Start.Name,
+				To = routeInfo.End.Name,
+				Content = routeInfo.Content,
+				MetaKeyWords = routeInfo.MetaKeyWords,
+				MetaDescription = routeInfo.MetaDescription,
+				DateView = date
+			});
+		}
+
+		/// <summary>
+		/// Search action.
+		/// </summary>
+		/// <param name="model">The model that contains search parameters.</param>
+		/// <returns>
+		/// Action result.
+		/// </returns>
+		/// <remarks>
+		/// GET: /Home/Search/model
+		/// </remarks>
+		public ActionResult Search(SearchViewModel model)
+		{
+			var routeInfo = this.homeService.GetRouteInfo(model.From, model.To);
+			
+			if (!string.IsNullOrWhiteSpace(routeInfo.Link))
+			{
+				return this.Redirect(routeInfo.Link + "?date=" + model.DateView);
+			}
+
+			model.Content = routeInfo.Content;
+			model.MetaKeyWords = routeInfo.MetaKeyWords;
+			model.MetaDescription = routeInfo.MetaDescription;
+			
+			return this.View(model);
+		}
+
+		/// <summary>
+		/// Form action.
+		/// </summary>
+		/// <returns>
+		/// Action result.
+		/// </returns>
+		/// <remarks>
+		/// GET: /Home/Form
+		/// </remarks>
+		public ActionResult Form()
+		{
+			var viewModel = new FormViewModel()
+			{
+				Content = this.homeService.GetApplicationFormText(),
+				OperatorCodes = this.commonService.GetOperatorCodes()
+			};
+
+			return this.View(viewModel);
+		}
+
+		/// <summary>
+		/// Form action.
+		/// </summary>
+		/// <param name="viewModel">The view model.</param>
+		/// <returns>
+		/// Action result.
+		/// </returns>
+		/// <remarks>
+		/// POST: /Home/Form
+		/// </remarks>
+		[HttpPost]
+		public ActionResult Form(FormViewModel viewModel)
+		{
+			var request = new CreateFormRequest()
+			{
+				CompanyName = viewModel.CompanyName,
+				Site = viewModel.Site,
+				AdditionalInfo = viewModel.AdditionalInfo,
+				Routes = viewModel.Routes,
+				SelectedOperactorCodes = viewModel.SelectedOperactorCodes,
+				Phones = viewModel.Phones
+			};
+
+			this.homeService.SendApplicationForm(request);
+
+			return new EmptyResult();
+		}
+
+		#endregion
+
+		#region Overriden Methods
+
+		/// <summary>
+		/// Called when an unhandled exception occurs in the action.
+		/// </summary>
+		/// <param name="filterContext">Information about the current request and action.</param>
+		protected override void OnException(ExceptionContext filterContext)
+		{
+			this.logger.LogException(filterContext.Exception, "Exception was caught in the Home Controller.");
+
+			filterContext.ExceptionHandled = true;
+			filterContext.Result = this.View("Error");
+		}
+
+		#endregion
+	}
+}
---
+++ HomeService.cs
@@ -0,0 +1,286 @@
+﻿namespace Project.BusinessComponents.Services
+{
+	#region Usings
+
+	using System;
+	using System.Collections.Generic;
+	using System.Diagnostics.Contracts;
+	using System.Linq;
+	using System.Text;
+
+	using Project.BusinessComponents.Interfaces;
+	using Project.BusinessComponents.Interfaces.Database;
+	using Project.BusinessComponents.Interfaces.Services;
+
+	using Project.BusinessEntities.Builders;
+	using Project.BusinessEntities.DataContracts;
+	using Project.BusinessEntities.DataContracts.Enumerations;
+	using Project.BusinessEntities.DataContracts.Routes;
+	using Project.BusinessEntities.Models;
+	using Project.BusinessEntities.Models.Constants;
+	using Project.BusinessEntities.Requests;
+
+	#endregion
+
+	/// <summary>
+	/// Implements the <see cref="IHomeService" /> interface.
+	/// </summary>
+	public class HomeService : IHomeService
+	{
+		#region Constans
+		
+		private const PhoneNumberFormat = "+375-({0})-{1}";
+		
+		#endregion
+		
+		#region Private Fields
+
+		/// <summary>
+		/// Reference to the <see cref="IHomeDatabaseService" /> implementer.
+		/// </summary>
+		private readonly IHomeDatabaseService databaseService;
+
+		/// <summary>
+		/// Reference to the <see cref="ILogger" /> implementer.
+		/// </summary>
+		private readonly ILogger logger;
+
+		/// <summary>
+		/// Reference to the <see cref="IMailService" /> implementer.
+		/// </summary>
+		private readonly IMailService mailService;
+
+		#endregion
+
+		#region Constructors
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="HomeService" /> class.
+		/// </summary>
+		/// <param name="databaseService">The database service.</param>
+		/// <param name="logger">The logger.</param>
+		/// <param name="mailService">The mail service.</param>
+		public HomeService(
+			IHomeDatabaseService databaseService,
+			ILogger logger,
+			IMailService mailService)
+		{
+			Contract.Requires(databaseService != null, "databaseService");
+			Contract.Requires(logger != null, "logger");
+			Contract.Requires(mailService != null, "mailService");
+
+			this.databaseService = databaseService;
+			this.logger = logger;
+			this.mailService = mailService;
+		}
+
+		#endregion
+
+		#region IHomeService Members
+
+		/// <summary>
+		/// Gets the home page landing.
+		/// </summary>
+		/// <returns>
+		/// Content of the home landing page.
+		///</returns>
+		public string GetHomePageLanding()
+		{
+			var homeArticle = this.databaseService.GetArticles(ArticleType.HomePageLanding).FirstOrDefault();
+
+			return homeArticle == null ? null : homeArticle.Content;
+		}
+
+		/// <summary>
+		/// Gets the published messages.
+		/// </summary>
+		/// <returns>
+		/// Collection of published messages.
+		/// </returns>
+		public IEnumerable<PublishedMessage> GetPublishedMessages()
+		{
+			return this.databaseService.GetPublishedMessages();
+		}
+
+		/// <summary>
+		/// Gets the articles.
+		/// </summary>
+		/// <param name="type">The type of the articles to be selected.</param>
+		/// <returns>
+		/// Collection of published articles.
+		/// </returns>
+		public IEnumerable<Article> GetArticles(ArticleType type)
+		{
+			return this.databaseService.GetArticles(type);
+		}
+
+		/// <summary>
+		/// Gets the article.
+		/// </summary>
+		/// <param name="type">The type of the article to be selected.</param>
+		/// <param name="name">The name of the article to be selected.</param>
+		/// <returns>
+		/// The article.
+		/// </returns>
+		public Article GetArticle(ArticleType type, string name)
+		{
+			return this.databaseService.GetArticle(type, name);
+		}
+
+		/// <summary>
+		/// Gets the route information.
+		/// </summary>
+		/// <returns>
+		/// Collection of route information entities.
+		/// </returns>
+		public IEnumerable<RouteInfo> GetRouteInfo()
+		{
+			return this.databaseService.GetRouteInfo();
+		}
+
+		/// <summary>
+		/// Gets the route information.
+		/// </summary>
+		/// <param name="name">The name of the route.</param>
+		/// <returns>
+		/// The route information.
+		/// </returns>
+		public RouteInfo GetRouteInfo(string name)
+		{
+			return this.databaseService.GetRouteInfo(name);
+		}
+
+		/// <summary>
+		/// Gets the route info.
+		/// </summary>
+		/// <param name="startName">The name of the start route point.</param>
+		/// <param name="endName">The name of the end route point.</param>
+		/// <returns>
+		/// The route info.
+		/// </returns>
+		public RouteInfo GetRouteInfo(string startName, string endName)
+		{
+			string routeName =
+				this.databaseService.GetRouteInfo()
+					.Where(t => t.Start.Name == startName && t.End.Name == endName)
+					.Select(t => t.Link)
+					.FirstOrDefault();
+
+			if (string.IsNullOrWhiteSpace(routeName))
+			{
+				return new RouteInfo();
+			}
+
+			return this.GetRouteInfo(routeName);
+		}
+
+		/// <summary>
+		/// Gets the contacts.
+		/// </summary>
+		/// <returns>
+		/// The contacts view.
+		/// </returns>
+		public Article GetContacts()
+		{
+			return this.databaseService.GetContacts();
+		}
+
+		/// <summary>
+		/// Gets the application form text.
+		/// </summary>
+		/// <returns>
+		/// The text of the application form.
+		/// </returns>
+		public string GetApplicationFormText()
+		{
+			IEnumerable<AdminSettings> adminSettings = this.databaseService.GetAdminSettings();
+
+			return GetSettingsValue(adminSettings, AdminSettingsKeys.CompanyFormText);
+		}
+
+		/// <summary>
+		/// Sends the application form.
+		/// </summary>
+		/// <param name="request">The request that contains data about creating application form.</param>
+		public void SendApplicationForm(CreateFormRequest request)
+		{
+			string phones = string.Join(";", GetPhoneNumbers(request.SelectedOperactorCodes, request.Phones));
+
+			var form = new Form()
+			{
+				DateTime = DateTime.Now.ToLocalTime(),
+				CompanyName = request.CompanyName,
+				AdditionalInfo = request.AdditionalInfo,
+				Site = request.Site,
+				Status = FormStatus.New,
+				Phones = phones,
+				Routes = string.Join(";", request.Routes)
+			};
+
+			this.databaseService.AddApplicationForm(form);
+
+			string text =
+				new StringBuilder()
+					.AppendLine(request.CompanyName)
+					.AppendLine(phones)
+					.AppendLine(request.Site)
+					.AppendLine(string.Join(";", request.Routes))
+					.AppendLine(request.AdditionalInfo)
+					.ToString();
+
+			this.mailService.SendMail(text);
+		}
+
+		#endregion
+
+		#region Private Methods
+
+		/// <summary>
+		/// Gets the settings value.
+		/// </summary>
+		/// <param name="settings">The settings.</param>
+		/// <param name="name">The name.</param>
+		/// <returns>
+		/// Settings value.
+		/// </returns>
+		private static string GetSettingsValue(IEnumerable<AdminSettings> settings, string name)
+		{
+			AdminSettings item = settings.FirstOrDefault(s => s.Name == name);
+
+			return item != null ? item.Value : string.Empty;
+		}
+
+		/// <summary>
+		/// Gets the key from pairs.
+		/// </summary>
+		/// <param name="settings">The settings.</param>
+		/// <param name="name">The name.</param>
+		/// <returns>
+		/// Company settings value.
+		/// </returns>
+		private static string GetSettingsValue(IEnumerable<CompanySettings> settings, string name)
+		{
+			CompanySettings item = settings.FirstOrDefault(s => s.Name == name);
+
+			return (item != null) ? item.Value : string.Empty;
+		}
+
+		/// <summary>
+		/// Gets the collection of phone numbers as concatenation of codes and phones.
+		/// </summary>
+		/// <param name="codes">The collection of codes.</param>
+		/// <param name="phones">The collection of phones.</param>
+		/// <returns>
+		/// Collection of phone numbers.
+		/// </returns>
+		private static IEnumerable<string> GetPhoneNumbers(IList<string> codes, IList<string> phones)
+		{
+			for (int i = 0; i < phones.Count(); i++)
+			{
+				yield return string.Format(PhoneNumberFormat, codes[i], phones[i]);
+			}
+		}
+
+		#endregion
+	}
+}
